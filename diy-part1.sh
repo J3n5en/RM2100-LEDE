f680fc2a58a4941b24eac6bc2eef99935a18a7f6
J3n5en <admin@J3n5en.com>
2020-05-05 00:01:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,18 +17,7 @@
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 git clone  https://github.com/jerrykuku/luci-app-vssr.git  package/lean/luci-app-vssr
 
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull origin master
-git branch --set-upstream-to=origin/master master
-echo $(pwd)
-echo $GITHUB_WORKSPACE
-cd ../..
-# 编译 po2lmo (如果有po2lmo可跳过)
-pushd package/luci-app-openclash/luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+rm -rf package/lean/luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
